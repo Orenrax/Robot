8a81baa42f8db69e940182ce25431ca89232f79f
Orenman <rx@mail.ru>
2022-09-07 22:02:46 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,155 @@
+import java.math.*;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        Robot robot = new Robot(10, -20, Direction.DOWN);
+//        moveRobot(robot, 0, 0);
+
+    }
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+    public static class Robot {
+        int x;
+        int y;
+        Direction dir;
+
+        public Robot(int x, int y, Direction dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+
+        public Direction getDirection() {
+            return dir;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void turnLeft() {
+            if (dir == Direction.UP) {
+                dir = Direction.LEFT;
+            } else if (dir == Direction.DOWN) {
+                dir = Direction.RIGHT;
+            } else if (dir == Direction.LEFT) {
+                dir = Direction.DOWN;
+            } else if (dir == Direction.RIGHT) {
+                dir = Direction.UP;
+            }
+        }
+
+        public void turnRight() {
+            if (dir == Direction.UP) {
+                dir = Direction.RIGHT;
+            } else if (dir == Direction.DOWN) {
+                dir = Direction.LEFT;
+            } else if (dir == Direction.LEFT) {
+                dir = Direction.UP;
+            } else if (dir == Direction.RIGHT) {
+                dir = Direction.DOWN;
+            }
+        }
+
+        public void stepForward() {
+            if (dir == Direction.UP) {
+                y++;
+            }
+            if (dir == Direction.DOWN) {
+                y--;
+            }
+            if (dir == Direction.LEFT) {
+                x--;
+            }
+            if (dir == Direction.RIGHT) {
+                x++;
+            }
+        }
+    }
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        // your code
+//            int x = robot.getX();
+//            int y = robot.getY();
+
+        System.out.println(robot.getX() + " " + robot.getY() + " " + robot.getDirection()+" start");
+//                  -10, 20
+        if (robot.getX() > toX) {
+            while (robot.getDirection() != (Direction.LEFT)) {
+                robot.turnRight();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        } else if (robot.getX() < toX) {
+            while (robot.getDirection() != (Direction.RIGHT)) {
+                robot.turnRight();
+            }
+            System.out.println(robot.getDirection());
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            } while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        } else if (robot.getY() < toY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            } while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
+
+
+        System.out.println(robot.getX() + " " + robot.getY() + " " + robot.getDirection()+" end");
+//            robot.turnRight();
+//            robot.stepForward();
+//            robot.stepForward();
+//            robot.stepForward();
+//            System.out.println(x+" "+y + " " + robot.getDirection());
+    }
+
+    class Person {
+
+        String name;
+        public String getName(){ return name; }
+
+        public Person(String name){
+
+            this.name=name;
+        }
+
+        public void display(){
+
+            System.out.println("Name: " + name);
+        }
+    }
+//    class Employee extends Person{
+//        public Employee(String name){
+//            super(name);    // если базовый класс определяет конструктор
+//            //  то производный класс должен его вызвать
+//        }
+//    }
+    //class Work {
+        Person Al = new Person("AL");
+
+    //}
+}
+
+
+
